97690de36f22979a5191ea9349cd86474bef484f
a-shved90 <a.shved90@gmail.com>
2017-05-13 00:24:32 +0100

    changed folder structure to reflect project name, fixed JS and added publisher img, toggle menu visibility

--- SimonEllice/site/scripts.js
+++ SimonEllice/site/scripts.js
@@ -10,14 +10,21 @@ var requesedNav = navigation.querySelectorAll("a[href='#" + requestedArticle + "
 //load next article once scrolled to bottom 400px
 document.addEventListener("scroll", function(event){
 
-    setInterval(function(){
-        if ((window.innerHeight + window.scrollY + 400) >= document.body.offsetHeight) {
-            var nextArticle = visibleArticles.length;
-            console.log(totalArticles[nextArticle])
-            totalArticles[nextArticle].classList.add("show");
-        }
-        isScrolledIntoView();
-    }, 1000)
+	setInterval(function(){
+		if ((window.innerHeight + window.scrollY + 400) >= document.body.offsetHeight) {
+			var nextArticle = visibleArticles.length;
+			console.log(totalArticles[nextArticle])
+			totalArticles[nextArticle].classList.add("show");
+		}
+		isScrolledIntoView();
+
+	}, 1000)
+
+	if (window.scrollY > 40) {
+		document.getElementsByClassName("logo")[0].classList.add("small");
+	} else {
+		document.getElementsByClassName("logo")[0].classList.remove("small");
+	}
 
 });
 
@@ -27,19 +34,20 @@ document.addEventListener("scroll", function(event){
 
 // open website on an article title based on url hash
 if(window.location.hash) {
-    for (i = 0; i < totalArticles.length; i++){
-        if(totalArticles[i].id == requestedArticle){
-            for (j = 0; j <= i; j++){
-                totalArticles[j].classList.add("show");
-            }
-            requesedNav.parentElement.classList.add("active");
-        }
-    }
-    console.log(requestedArticle)
-    document.querySelector("#" + requestedArticle).scrollIntoView({ behavior: 'smooth' });
+	for (i = 0; i < totalArticles.length; i++){
+		if(totalArticles[i].id == requestedArticle){
+			for (j = 0; j <= i; j++){
+				totalArticles[j].classList.add("show");
+			}
+			navigation.parentElement.classList.remove("active");
+			requesedNav.parentElement.classList.add("active");
+		}
+	}
+	console.log(requestedArticle)
+	document.querySelector("#" + requestedArticle).scrollIntoView({ behavior: 'smooth' });
 
 } else {
-    console.log("no hash")
+	console.log("no hash")
 }
 
 
@@ -51,63 +59,72 @@ if(window.location.hash) {
 
 (function(){
 
-    var selector, elems, makeActive;
-    selector = 'nav li';
-    elems = document.querySelectorAll(selector);
+	var selector, elems, makeActive;
+	selector = 'nav li';
+	elems = document.querySelectorAll(selector);
 
 
 
 
-    makeActive = function () {
-        for (var i = 0; i < elems.length; i++){
-            elems[i].classList.remove('active');
-        }
-        
-        this.classList.add('active');
+	makeActive = function () {
+		for (var i = 0; i < elems.length; i++){
+			elems[i].classList.remove('active');
+		}
+		
+		this.classList.add('active');
 
-        var currentLink = this.getElementsByTagName("a")[0].getAttribute("href").split("#")[1];
+		var currentLink = this.getElementsByTagName("a")[0].getAttribute("href").split("#")[1];
 
-        for (i = 0; i < totalArticles.length; i++){
-            if(totalArticles[i].id == currentLink){
-                for (j = 0; j <= i; j++){
-                    totalArticles[j].classList.add("show");
-                    console.log("#" + currentLink)
-                    document.querySelector("#" + currentLink).scrollIntoView({ behavior: 'smooth' });
-                }
-            } else if (currentLink == "home"){
-                document.querySelector("#home").scrollIntoView({ behavior: 'smooth' });
-            }
-        }
-        
-    };
+		for (i = 0; i < totalArticles.length; i++){
+			if(totalArticles[i].id == currentLink){
+				for (j = 0; j <= i; j++){
+					totalArticles[j].classList.add("show");
+					console.log("#" + currentLink)
+					document.querySelector("#" + currentLink).scrollIntoView({ behavior: 'smooth' });
+				}
+			} else if (currentLink == "home"){
+				document.querySelector("#home").scrollIntoView({ behavior: 'smooth' });
+			}
+		}
+		
+	};
 
-    for (var i = 0; i < elems.length; i++) {
-        elems[i].addEventListener('mousedown', makeActive);
-    }
+	for (var i = 0; i < elems.length; i++) {
+		elems[i].addEventListener('mousedown', makeActive);
+	}
 
 })();
 
 
 
-var scroll = true;
 
 
 
 function isScrolledIntoView() {
-    for (i = 0; i < totalArticles.length; i++){
-        var elemTop = totalArticles[i].getBoundingClientRect().top;
-        var elemBottom = totalArticles[i].getBoundingClientRect().bottom;
-        var isVisible = (elemTop <= 0) && (elemBottom >= window.innerHeight);
-        var currentLink = navigation.querySelectorAll("a[href='#" + totalArticles[i].id + "']")[0];
-        if(isVisible){
-            currentLink.parentElement.classList.add("active");
-            // window.location.hash = totalArticles[i].id;
-            // window.addEventListener("hashchange",function(event){
-            //     event.preventDefault();
-            // });
+	for (i = 0; i < totalArticles.length; i++){
+		var elemTop = totalArticles[i].getBoundingClientRect().top;
+		var elemBottom = totalArticles[i].getBoundingClientRect().bottom;
+		var isVisible = (elemTop <= window.innerHeight) && (elemBottom >= window.innerHeight);
+		var currentLink = navigation.querySelectorAll("a[href='#" + totalArticles[i].id + "']")[0];
+		if(isVisible){
+			//console.log(totalArticles[i])
+			navigation.parentElement.classList.remove("active");
+			currentLink.parentElement.classList.add("active");
 
-        } else {
-            currentLink.parentElement.classList.remove("active");
-        }
-    }
+		} else {
+			currentLink.parentElement.classList.remove("active");
+		}
+	}
 }
+
+
+document.addEventListener("click", function(event){
+	var morocco = document.getElementById("morocco");
+	if(event.target == morocco || event.target == morocco.getElementsByTagName("img")[0]){
+		if(!navigation.classList.contains("visible") == true){
+			navigation.classList.add("visible");
+		} else{
+			navigation.classList.remove("visible");
+		}
+	}
+})
